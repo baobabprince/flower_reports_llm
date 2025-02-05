a3e010fc69f6894afcbad54338910182891ef03d
baobabprince <46530629+baobabprince@users.noreply.github.com>
2025-02-05 13:28:53 +0200

    ;

--- static/js/FlowerMap.js
+++ static/js/FlowerMap.js
@@ -46,7 +46,7 @@ class FlowerMap {
         
 
         //Replace with your custom style URL from MapTiler:
-        const customStyleUrl = 'https://api.maptiler.com/maps/eab1918b-d2ce-41e0-abfb-e50d9c8d7a90/style.json?key=ToTdsblYAzP3SYjjFYmo'
+        const customStyleUrl = 'https://api.maptiler.com/maps/eab1918b-d2ce-41e0-abfb-e50d9c8d7a90/style.json?key=ToTdsblYAzP3SYjjFYmo';
         L.tileLayer(customStyleUrl, {
             attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>'
         }).addTo(this.map);
